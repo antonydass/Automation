1bd58782ca35158a084a339c02140383230345b6
antonydass <antony602@gmail.com>
2016-09-28 14:53:11 +0530

    New code commit
    For additional sys doc

--- src/com/app/main/Forbranch.java
+++ src/com/app/main/Forbranch.java
@@ -1,10 +1,10 @@
 package com.app.main;
 
 public class Forbranch {
-	// This is for updating purpose 
-	// We are going to keep this is update purpsoe
+
 	
 	public static void main(String args[]){
 		System.out.println("Nice");
+		system.out.println("this looks nice")
 	}
 	}
