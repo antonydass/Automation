7aefe465d620670edf804375b9978dc8a66a99b2
antonydass <antony602@gmail.com>
2016-09-28 14:14:51 +0530

    With command line commit !

--- src/com/app/main/Fun.java
+++ src/com/app/main/Fun.java
@@ -1,5 +1,6 @@
 package com.app.main;
-
+// This is for updating purpose 
+// We are going to keep this is update purpsoe
 public class Fun {
 public static void main(String args[]){
 	System.out.println("Nice");
